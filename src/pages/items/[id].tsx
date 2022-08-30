37934d97b09e4291b1fb73119abe69f2733b0505
Kazuaki Kuwahara <kazuaki.kuwahara@rakus-partners.co.jp>
2022-08-30 11:14:21 +0900

    create-itemdetail

--- src/pages/items/[id].tsx
+++ src/pages/items/[id].tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { Item } from '../../types';
+import { useState } from 'react';
 
 export async function getStaticPaths() {
   const res = await fetch('http://localhost:8000/items');
@@ -7,7 +9,6 @@ export async function getStaticPaths() {
   const paths = number.map((num: any) => {
     return { params: { id: num.id.toString() } };
   });
-  console.log('paths', paths);
   return {
     paths,
     fallback: true,
@@ -17,7 +18,6 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params }: any) {
   const res = await fetch(`http://localhost:8000/items/${params.id}`);
   const detail = await res.json();
-  console.log('detail', detail);
   if (!detail.id) {
     return { notFound: true };
   } else {
@@ -29,55 +29,40 @@ export async function getStaticProps({ params }: any) {
 }
 
 const ItemData = ({ detail }: any) => {
-  let remove = 0;
-  const radioDeselect = (already: any, numeric: any) => {
-    if (remove == numeric) {
-      already = false;
-      remove = 0;
-    } else {
-      remove = numeric;
-    }
-  };
+  const [num, setNum] = useState(1)
+  let total = num * detail.price
   return (
     <>
       <Head>
         <title>商品詳細</title>
       </Head>
       <div>
-        <p>{detail.name}</p>
+        <h1>{detail.name}</h1>
         <p>{detail.description}</p>
-        <Image src={detail.image_path} width='300px' height='300px' alt={detail.description} />
+        <Image
+          src={detail.image_path}
+          width="300px"
+          height="300px"
+          alt={detail.description}
+        />
+        <p>金額：{detail.price}円（税込）</p>
       </div>
-      <form>
-        <input
-          type="radio"
-          id="M"
-          name="Msize"
-          value="1"
-          onClick={() => radioDeselect(this, 1)}
-          checked
-        />
-        <label htmlFor="Msize">M {detail.priceM}円</label>
-        <input
-          type="radio"
-          id="L"
-          name="Lsize"
-          value="2"
-          onClick={() => radioDeselect(this, 2)}
-        />
-        <label htmlFor="Lsize">L {detail.priceL}円</label>
-        <br />
+      <form name="info">
         <p>
-          数量
-          <select name="quantity">
+          数量：
+          <select name="quantity" onChange={(e: any) => setNum(e.target.value)}>
             <option value="1" selected>
               1
             </option>
             <option value="2">2</option>
             <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
           </select>
+          {' '}個
         </p>
       </form>
+      <p>この商品の金額：{total}円（税込）</p>
     </>
   );
 };
