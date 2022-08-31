fecea8b223e38a7d8cf3e06003af4b187033e386
sasaki mai <mai.sasaki@rakus-partners.co.jp>
2022-08-31 17:56:34 +0900

    ログインログアウト作成（途中）

--- src/pages/users/logout.tsx
+++ src/pages/users/logout.tsx
@@ -1,20 +1,20 @@
 // https://swr.vercel.app/ja/docs/mutation
-// ↑から一旦持ってきた。
+// ↑から一旦持ってきた。使えるかわかんない
 // 次の例では、ユーザーが "Logout" ボタンをクリックしたときに、ログイン情報 （ たとえば <Profile/> の中身 ）を自動的に取得する方法を示します。
 
 import useSWR, { useSWRConfig } from 'swr'
 
-function App () {
+export default function Logout () {
   const { mutate } = useSWRConfig()
-
   return (
     <div>
+      {/* ここに取得するコンポーネント入れたい */}
       <button onClick={() => {
-        // クッキーを期限切れとして設定します
-        document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
-
-        // このキーを使用してすべての SWR に再検証するように指示します
-        mutate('/api/user')
+        // クッキーを期限切れとして設定
+        const date =new Date();
+        document.cookie = `id=; expires=Thu, expires=${date.toUTCString} path=/;`
+        // このキーを使用してすべての SWR に再検証
+        mutate('http://localhost:8000/users');
       }}>
         Logout
       </button>
