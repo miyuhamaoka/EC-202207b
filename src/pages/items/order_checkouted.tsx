d062191fa53b50ff8031bb647f5c044fb7056030
秋元崇利 <takatoshi.akimoto@rakus-pertners.co.jp>
2022-09-02 11:31:54 +0900

    注文決済画面の追加1-7

---
+++ src/pages/items/order_checkouted.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/Link';
+
+export default function button() {
+  return (
+    <>
+      <div>
+        <h1>決済が完了しました！</h1>
+        <br />
+        <p>この度はご注文ありがとうございます。</p>
+        <p>
+          お支払い先は、お送りしたメールに記載してありますのでご確認ください。
+        </p>
+        <p>メールが届かない場合は「注文履歴」からご確認ください。</p>
+      </div>
+      <div>
+        <button type="submit">
+          <Link href="/">
+            <a>トップ画面を表示する</a>
+          </Link>
+        </button>
+      </div>
+    </>
+  );
+}
