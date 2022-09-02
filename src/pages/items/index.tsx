b53a422c5063aba3aee70aa3ed828c68916ead5b
sasaki mai <mai.sasaki@rakus-partners.co.jp>
2022-09-03 01:11:20 +0900

    商品一覧ページの遷移修正

--- src/pages/items/index.tsx
+++ src/pages/items/index.tsx
@@ -1,35 +1,39 @@
 import Head from 'next/head';
 import Link from 'next/Link';
-import Items from '../../components/ItemList';
+import Items from '../../components/itemList';
 import Layout from '../../components/layout';
 import {User} from '../../types'
 // import {GetStaticPaths} from 'next';
 // import { GetStaticProps } from 'next';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 
 export default function Home() {
-  const cookies =document.cookie;
-  const cookiesArray = cookies.split('; ');
+  const [cookiesArray,setCookiesArray]:[string[],Function]=useState([]);
+  useEffect(()=>{
+    const cookies = document.cookie;
+    const array = cookies.split(';');
+    if(array!==undefined){
+      setCookiesArray(array)
+    }
+  },[])
+
   return (
     <>
+      <Head>
+        <title>ラクラクカフェ！</title>
+        <Link rel="icon" href="/favicon.ico" />
+      </Head>
       <Layout />
     {cookiesArray.map((cookie)=>{
       const cookieArray = cookie.split('=');
-      if (cookieArray[0] === 'name') {
-      return(
-        <p>{`こんにちは${cookieArray.slice(1)}`}さん</p>
-        )
-        }else{
-          <p>ログインをしてください</p>
+      if ( cookieArray[0] === ' name') {
+        return(
+          // eslint-disable-next-line react/jsx-key
+          <p>{`こんにちは${cookieArray.slice(1)}`}さん</p>
+          )
         }
-    })}
-      <Head>
-        <title>ラクラクカフェ！</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-       
-
+      })}
       <Link href="/items">
         <a>商品一覧</a>
       </Link>
@@ -42,37 +46,9 @@ export default function Home() {
       <Link href="/items/order_comfirm">
         <a>注文履歴</a>
       </Link>
-
       <div>
         <Items></Items>
       </div>
     </>
   );
 }
-
-
- // const cookies = document.cookie;
-  // const array = cookies.split(';');
-
-
-  // fetch(`http://localhost:8000/users`)
- 
- 
-//   const cookies=document.cookie;
-
-//   const response = await fetch(
-//     `http://localhost:8000/users`,
-//     {
-//       method: 'GET',
-//       headers: {
-//         Accept: 'application/json',
-//       },
-//       credentials: 'include',
-//     }
-//   );
-//   const users:User = await response.json();
-// return(
-//   <>
-//   <Home users={users}/>
-//   </>
-// )
