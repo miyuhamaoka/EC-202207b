2fe33a377ab16194bed36ee7672e4d3bdb1d52c9
sasaki mai <mai.sasaki@rakus-partners.co.jp>
2022-09-07 15:34:58 +0900

    スタイル変更

--- src/pages/items/cart.tsx
+++ src/pages/items/cart.tsx
@@ -6,6 +6,7 @@ import Router from 'next/router';
 import { CartItem } from '../../types';
 import Layout from '../../components/layout';
 import Image from 'next/image';
+import cartStyle from '../../styles/cart.module.css';
 
 export async function getServerSideProps({ query }: any) {
   // console.log(
@@ -16,7 +17,7 @@ export async function getServerSideProps({ query }: any) {
     `http://localhost:8000/cartItems?userId=${query.userId}` //クエリパラメーターのuserId取り出す
   );
   const cartItems = await res.json();
-  
+
   //length=1は登録してある(買い物したことある)人。カートの[0]番目を持ってくる
   //elseはユーザー登録したことない人は[]を用意
   let cartItem: Omit<CartItem, 'id'>;
@@ -38,14 +39,15 @@ const CartPage = ({ cartItem }: { cartItem: CartItem }) => {
   //cartItemsのitemsから削除ボタンが押されたitemIdの商品を除いてcartitemをbodyにセットする
   //(idが一致したものを削除?)
   const onClickDelete = async (itemId: number) => {
-
     //cartItemsのitemsのitemIdだけ抜く
-    const restItems = cartItem.items.filter(item => item.id !== itemId);
+    const restItems = cartItem.items.filter(
+      (item) => item.id !== itemId
+    );
 
     const body = {
       id: cartItem.id,
       userId: cartItem.userId,
-      items: restItems
+      items: restItems,
     };
     // console.log(restItems);
 
@@ -54,9 +56,8 @@ const CartPage = ({ cartItem }: { cartItem: CartItem }) => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
     };
-
 
     const response = await fetch(
       `http://localhost:8000/cartItems/${cartItem.id}`,
@@ -65,13 +66,12 @@ const CartPage = ({ cartItem }: { cartItem: CartItem }) => {
     console.log(response.json);
 
     //削除後遷移
-    Router.push(`/items/cart?userId=${cartItem.userId}`)
+    Router.push(`/items/cart?userId=${cartItem.userId}`);
   };
-
 
   //消費税と商品合計に使用する価格の合計を取得 (商品無くなったら0がセットされる)
   const itemPrice = cartItem.items.map((e: any) => e.price);
-  const total = itemPrice.reduce((a: number, b: number) => a + b,0);
+  const total = itemPrice.reduce((a: number, b: number) => a + b, 0);
 
   //商品の表示
   //カートに商品がある時ない時(ない時はlength0でタグ非表示)
@@ -79,62 +79,77 @@ const CartPage = ({ cartItem }: { cartItem: CartItem }) => {
     if (cartItem.items.length > 0) {
       return (
         <>
-        <table>
-        <thead>
-          <tr>
-            <th>商品名</th>
-            <th>価格（税抜）</th>
-            <th>数量</th>
-            <th>小計</th>
-            <td></td>
-          </tr>
-        </thead>
-        <tbody>
-          {cartItem.items.map((item: any) => (
-            <tr key={item.id}>
-              <td>
-                <Image src={item.image_path} width={100} height={100} alt={item.name} />
-                <p>{item.name}</p>
-              </td>
-              <td>{item.price}円</td>
-              <td>{item.quantity}個</td>
-              <td>{item.price}円</td>
-              <td>
-                <button onClick={() => onClickDelete(item.id)}>
-                  削除
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <h3>消費税:{Math.round(total * 0.08)}円</h3>
-      <h2>ご注文金額合計:{Math.round(total * 1.08)}円（税込）</h2>
+          <div className={cartStyle.tbody}>
+            <table className={cartStyle.table}>
+              <thead className={cartStyle.thead}>
+                <tr className={cartStyle.tr}>
+                  <th className={cartStyle.th}>商品名</th>
+                  <th className={cartStyle.th}>価格（税抜）</th>
+                  <th className={cartStyle.th}>数量</th>
+                  <th className={cartStyle.th}>小計</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItem.items.map((item: any) => (
+                  <tr key={item.id} className={cartStyle.tr}>
+                    <td className={cartStyle.th}>
+                      <Image
+                        src={item.image_path}
+                        width={200}
+                        height={200}
+                        alt={item.name}
+                      />
+                      <p>{item.name}</p>
+                    </td>
+                    <td className={cartStyle.th}>{item.price}円</td>
+                    <td className={cartStyle.th}>{item.quantity}個</td>
+                    <td className={cartStyle.th}>{item.price}円</td>
+                    <td>
+                      <button className={cartStyle.dbtn} onClick={() => onClickDelete(item.id)}>
+                        削除
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <div className={cartStyle.price}>
+            <h3>消費税:{Math.round(total * 0.08)}円</h3>
+            <h2>
+              ご注文金額合計:{Math.round(total * 1.08)}円（税込）
+            </h2>
+            </div>
 
-      <Link href="/items/order_confirm">
-        <button type="button">注文に進む</button>
-      </Link>
-      </>
+            <Link href="/items/order_confirm">
+              <button className={cartStyle.btn} type="button">注文に進む</button>
+            </Link>
+          </div>
+        </>
       );
     } else {
-      return (
-        <p>カートに商品がありません</p>
-      );
+      return <p>カートに商品がありません</p>;
     }
   }
- 
+
   return (
     <>
       <Head>
         <title>ショッピングカート</title>
       </Head>
       <Layout />
-      <h1>ショッピングカート</h1>
+      <h1 className={cartStyle.h1}>ショッピングカート</h1>
 
       <Render />
-
     </>
   );
 };
 
 export default CartPage;
+
+
+
+
+
+
+
+
